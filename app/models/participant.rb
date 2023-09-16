3291cd686e2235eb6c77b82b6355239c1d76dc83
Manuel Farez <manufarez@gmail.com>
2023-09-15 23:46:47 -0500

    implements new participants list

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -5,4 +5,13 @@ class Participant < ApplicationRecord
   has_many :made_payments, class_name: 'Payment', dependent: :nullify
 
   delegate :name, to: :user
+
+  def paid_next_participant?(cadena, current_date)
+    next_participant = cadena.next_paid_participant(current_date)
+    if next_participant == self
+      true
+    else
+      Payment.where(user: user, participant: next_participant).present?
+    end
+  end
 end
