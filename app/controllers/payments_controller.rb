3cd0e4d8eb4e387ee297848f3802d8f86fe8489e
Manuel Farez <manufarez@gmail.com>
2023-09-25 18:34:42 -0500

    Adds funding before payment processing validation

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,34 +2,28 @@ class PaymentsController < ApplicationController
   before_action :find_cadena
 
   def create
-    @payment = Payment.new(
-      cadena: @cadena,
-      participant_id: params[:participant_id],
-      amount: @cadena.installment_value,
-      user: current_user,
-      paid_at: global_date
-    )
+    @payment = Payment.new(cadena: @cadena, participant_id: params[:participant_id], amount: @cadena.installment_value, user: current_user, paid_at: global_date)
+    @next_paid_participant = @cadena.next_paid_participant(global_date) if @cadena.started?
     respond_to do |format|
-      if process_payment
+      if @payment.valid?
+        process_payment
         format.html { redirect_to cadena_url(@payment.cadena), notice: t('cadena.payment_success') }
         format.json { render json: @payment.cadena, status: :created, location: @payment.cadena }
       else
-        format.html { render @cadena, status: :unprocessable_entity }
+        format.html { redirect_to cadena_url(@payment.cadena), notice: @payment.errors.full_messages.to_sentence }
         format.json { render json: @payment.cadena.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def process_payment
-    return false unless @payment.valid?
-
     ActiveRecord::Base.transaction do
       sender = @payment.user
       receiver = @payment.participant
       sender.balance -= @payment.amount
       receiver.user.balance += @payment.amount
       receiver.payments_received += 1
-      @payment.save && sender.save && receiver.save
+      @payment.save(validate: false) && sender.save && receiver.save
     end
   end
 
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,13 +3,20 @@ class Payment < ApplicationRecord
   belongs_to :participant
   belongs_to :user
 
+  validate :sufficient_balance
   validate :max_payments
 
   private
 
+  def sufficient_balance
+    return unless amount > user.balance
+
+    errors.add(:base, :insufficient_funds)
+  end
+
   def max_payments
     return unless participant.present? && participant.payments_received > participant.payments_expected
 
-    errors.add(:payments_received, "can't exceed payments_expected")
+    errors.add(:amount, "can't exceed payments_expected")
   end
 end
