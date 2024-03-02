97ea967eae740ef4a4bacbe0a9666f1342b670c0
Manuel Farez <manufarez@gmail.com>
2024-03-02 17:09:46 -0500

    Adds new image format to e-mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'cadenapp.com' }
   config.action_mailer.delivery_method = :postmark
+  config.action_mailer.asset_host = 'https://www.cadenapp.com'
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
