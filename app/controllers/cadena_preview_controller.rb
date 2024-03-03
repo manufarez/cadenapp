a1ea827e896b31a9cb7d6be1e19b7c3eb714b54e
Manuel Farez <manufarez@gmail.com>
2024-03-03 10:30:37 -0500

    Hides cadena preview if invitation has been declined

--- app/controllers/cadena_preview_controller.rb
+++ app/controllers/cadena_preview_controller.rb
@@ -1,10 +1,13 @@
 class CadenaPreviewController < ApplicationController
   def show
     @invitation = Invitation.find_by(token: params[:token])
-    if @invitation && !@invitation.accepted
+    if @invitation && @invitation.accepted.nil?
       @cadena = @invitation.cadena
     elsif @invitation&.accepted || @invitation&.older_than_1h
       flash[:error] = t('notices.cadena.invitation.expired')
+      redirect_to root_path
+    elsif @invitation.accepted == false
+      flash[:error] = t('notices.cadena.invitation.declined')
       redirect_to root_path
     else
       flash[:error] = t('notices.cadena.invitation.token_error')
