486d92931ccfebb803790fc4f58ca5d1a9e99f19
Manuel Farez <manufarez@gmail.com>
2024-05-23 14:52:03 -0500

    Adds Turnstile to Newsletter subscribers

--- app/controllers/newsletter_subscribers_controller.rb
+++ app/controllers/newsletter_subscribers_controller.rb
@@ -1,10 +1,11 @@
 class NewsletterSubscribersController < ApplicationController
   def create
     @lead = NewsletterSubscriber.new(lead_params)
-    if @lead.valid?
+    if @lead.valid? && valid_captcha?(model: @lead)
       NewsletterSubscriber.link_to_user(@lead)
       redirect_to root_path, notice: t('notices.newsletter.subscribed')
     else
+      @lead.errors.details
       redirect_to root_path, notice: t('notices.newsletter.subscription_error')
     end
   end
