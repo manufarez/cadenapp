116b5ed6ff273c91ce2a5a44857d9f737c416462
Manuel Farez <manufarez.work@gmail.com>
2025-04-01 15:41:10 -0500

    Turnstile

--- config/initializers/turnstile.rb
+++ config/initializers/turnstile.rb
@@ -1,7 +1,7 @@
 Turnstile.configure do |config|
-  if Rails.env.development?
-    config.enabled = false
+  if Rails.env.production?
+    config.enabled = true
+    config.site_key = Rails.application.credentials.turnstile.site_key
+    config.secret_key = Rails.application.credentials.turnstile.secret_key
   end
-  config.site_key = Rails.application.credentials.turnstile.site_key
-  config.secret_key = Rails.application.credentials.turnstile.secret_key
 end
